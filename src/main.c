8030b95c11646fa5e5e8fa14a9ac4d2a48b42845
Zakharov <zahar80@yandex.ru>
2021-02-23 12:37:15 +0300

    Refactor exit program

--- src/main.c
+++ src/main.c
@@ -2,24 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+void exit_program(const char* error) {
+    fprintf(stderr, "%s\n", error);
+    exit(1);
+}
+
 int main(int argc, char** argv) {
     if (argc != 4) {
-        fprintf(stderr, "Args must be: input file, charset, output file\n");
-        exit(1);
+        exit_program("Args must be: input file, charset, output file");
     }
 
     const char* cp = argv[2];
     if (strcmp(cp, "cp1251") && strcmp(cp, "iso8859")) {
-        fprintf(stderr, "Coding page must be cp1251 or iso8859\n");
-        exit(1);
+        exit_program("Coding page must be cp1251 or iso8859");
     }
 
     FILE* in = fopen(argv[1], "r");
     FILE* out = fopen(argv[3], "w");
 
     if (in == NULL || out == NULL) {
-        fprintf(stderr, "Open file error\n");
-        exit(1);
+        exit_program("Open file error");
     }
 
     int ch;
